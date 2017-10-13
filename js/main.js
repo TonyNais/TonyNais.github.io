19e4389bffe99aa26ba7411b753ee53790260b75
tonynais <tonynmaina@gmail.com>
2017-10-13 07:19:21 +0300

    Perfect scroll dissable

--- js/main.js
+++ js/main.js
@@ -52,9 +52,9 @@ $('.map').on('mouseleave', function(){
 
 
 //==============___Scrollbars___================
-$('.section-vcardbody').perfectScrollbar({
-  wheelSpeed: 0.9
-});
+//$('.section-vcardbody').perfectScrollbar({
+//  wheelSpeed: 0.9
+//});
 
 //==============___Menu & Pages Animation___================
 
